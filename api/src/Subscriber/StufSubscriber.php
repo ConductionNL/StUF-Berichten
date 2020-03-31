1b398a38af5bc2a2c070ebdaea124987c35c353e
Robert Zondervan <Robert@conduction.nl>
2020-03-31 14:35:22 +0200

    Remove pay subscribers, add STUFsubscriber

--- api/src/Subscriber/StufSubscriber.php
+++ api/src/Subscriber/StufSubscriber.php
@@ -24,29 +24,34 @@ use Symfony\Component\HttpKernel\KernelEvents;
 use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Component\Yaml\Yaml;
 
-class PaymentSubscriber implements EventSubscriberInterface
+use Twig_Environment as Environment;
+
+class StufSubscriber implements EventSubscriberInterface
 {
     private $params;
     private $em;
     private $serializer;
     private $client;
-    public function __construct(ParameterBagInterface $params, EntityManagerInterface $em, SerializerInterface $serializer)
+    private $templating;
+
+    public function __construct(ParameterBagInterface $params, EntityManagerInterface $em, SerializerInterface $serializer, Environment $twig)
     {
 
         $this->params = $params;
         $this->em = $em;
         $this->serializer = $serializer;
         $this->client = new Client();
+        $this->templating = $twig;
     }
 
     public static function getSubscribedEvents()
     {
         return [
-            KernelEvents::REQUEST => ['payment', EventPriorities::PRE_DESERIALIZE],
+            KernelEvents::REQUEST => ['stuf', EventPriorities::PRE_DESERIALIZE],
         ];
     }
 
-    public function payment(RequestEvent $event)
+    public function stuf(RequestEvent $event)
     {
         //$result = $event->getControllerResult();
         $method = $event->getRequest()->getMethod();
@@ -66,45 +71,22 @@ class PaymentSubscriber implements EventSubscriberInterface
                 $renderType = 'jsonhal';
                 break;
             default:
-                $contentType = 'application/json';
-                $renderType = 'json';
+                $contentType = 'application/ld+json';
+                $renderType = 'jsonld';
         }
         //var_dump($route);
-        if($route=='api_payments_post_webhook_collection'){
-            //var_dump('a');
-            $providerId = $event->getRequest()->query->get('provider');
-           //var_dump($providerId);
-            $provider = $this->em->getRepository('App\Entity\Service')->find($providerId);
+        $request = json_decode($event->getRequest()->request,true);
 
-            $paymentId = $event->getRequest()->request->get('id');
-            //var_dump($paymentId);
+        $destination = $request['destination'];
+        $headers = $request['headers'];
+        $dataset = $request['dataset'];
+        $template = $request['template'];
 
+        $template = $this->templating->createTemplate('stuf/'.$template);
+        $message = $template->render($dataset);
 
-            if($provider instanceof Service && $provider->getType() == 'mollie'){
-                $mollieService = new MollieService($provider);
-                $payment = $mollieService->updatePayment($paymentId, $provider, $this->em);
-            }
-            else{
-                return;
-            }
-        }
-        else{
-            return;
-        }
+        ;
 
-
-        if($payment){
-            $this->em->persist($payment);
-            $this->em->flush();
-            $json = $this->serializer->serialize(
-                $payment,
-                $renderType, ['enable_max_depth'=>true]
-            );
-        }else{
-            $json = $this->serializer->serialize(
-                ["Error"=>"The payment is not related to an invoice in our database"], $renderType, ['enable_max_depth'=>true]
-            );
-        }
 
         // Creating a response
         $response = new Response(
